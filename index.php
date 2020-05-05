3151d956af56c79d5fa8795470b32ad8a0e13484
heok <heok.ca@gmail.com>
2020-05-05 13:41:12 -0400

    clean up

--- index.php
+++ index.php
@@ -1,23 +1,3 @@
 <?php
 
-    require "setup.php";
-
-
-//s?#-K;#"&6S0Msa:
-
-    use Psr\Http\Message\ResponseInterface as Response;
-    use Psr\Http\Message\ServerRequestInterface as Request;
-
-    use Slim\Views\Twig;
-
-    //TODO
-
-
-    $app->get('/', function (Request $request, Response $response) {
-        $view = Twig::fromRequest($request);
-        return $view->render($response, "index.html.twig");
-    });
-
-
-    $app->run();
-
+(require __DIR__ . '/config/bootstrap.php')->run();
