bee1a17ebae4fde7427857068255b9d5772475eb
W <futurename@gmail.com>
2020-05-11 17:16:53 -0400

    passing price info to filters on car_selection page

--- config/routes.php
+++ config/routes.php
@@ -3,8 +3,8 @@
 
     namespace App\Controller;
 
-use App\Middleware\AuthMiddleware;
-use DB;
+    use App\Middleware\AuthMiddleware;
+    use DB;
     use Slim\App;
     use Psr\Http\Message\ResponseInterface as Response;
     use Psr\Http\Message\ServerRequestInterface as Request;
@@ -31,7 +31,7 @@ use DB;
         })->add(AuthMiddleware::mustNotLogin());
 
 
-        $app->group('/admin', function(RouteCollectorProxy $group) {
+        $app->group('/admin', function (RouteCollectorProxy $group) {
             $group->get('', AdminController::class . ':home');
             $group->get('/stores', AdminController::class . ':storeList');
             $group->get('/cartypes', AdminController::class . ':carTypeList');
@@ -52,15 +52,15 @@ use DB;
             $response = $response->withHeader('Content-type', 'application/json; charset=UTF-8');
 
             $jsonText = $request->getBody()->getContents();
-            
-            $data = json_decode($jsonText,true);
+
+            $data = json_decode($jsonText, true);
 
             $searchText = $data['searchText'];
 
-            $secondChar = substr($searchText, 1,1);
-            if(ctype_digit($secondChar) && strlen($searchText) <=3) {
+            $secondChar = substr($searchText, 1, 1);
+            if (ctype_digit($secondChar) && strlen($searchText) <= 3) {
                 $result = DB::query("SELECT * FROM cities WHERE postalCode like %s", $searchText . '%');
-            }else{
+            } else {
                 $result = DB::query("SELECT * FROM cities WHERE name like %s", $searchText . '%');
             }
 
@@ -69,40 +69,67 @@ use DB;
             return $response;
         });
 
-        $app->post('/car_selection', function (Request $request, Response $response, array $args){
+        $app->post('/car_selection', function (Request $request, Response $response, array $args) {
             $view = Twig::fromRequest($request);
 
             $allVehicles = DB::query("SELECT * FROM cartypes");
+            $carMinPrice = DB::query("SELECT MIN(dailyPrice) as 'min' from cartypes WHERE category = %s", "Car")[0]['min'];
+            $suvMinPrice = DB::query("SELECT MIN(dailyPrice) as 'min' from cartypes WHERE category = %s", "SUV")[0]['min'];
+            $vanMinPrice = DB::query("SELECT MIN(dailyPrice)  as 'min' from cartypes WHERE category = %s", "Van")[0]['min'];
+            $truckMinPrice = DB::query("SELECT MIN(dailyPrice)  as 'min' from cartypes WHERE category = %s", "Truck")[0]['min'];
 
-            return $view->render($response, 'car_selection.html.twig',[
-                'allVehicles'=>$allVehicles
+            $pass2 = DB::query("SELECT MIN(dailyPrice) as 'min' FROM cartypes WHERE passengers >= 2")[0]['min'];
+            $pass4 = DB::query("SELECT MIN(dailyPrice) as 'min' FROM cartypes WHERE passengers >= 4")[0]['min'];
+            $pass5 = DB::query("SELECT MIN(dailyPrice) as 'min' FROM cartypes WHERE passengers >= 5")[0]['min'];
+            $pass7 = DB::query("SELECT MIN(dailyPrice) as 'min' FROM cartypes WHERE passengers >= 7")[0]['min'];
+
+            $bag3 = DB::query("SELECT MIN(dailyPrice) as 'min' FROM cartypes WHERE bags >= 3")[0]['min'];
+            $bag4 = DB::query("SELECT MIN(dailyPrice) as 'min' FROM cartypes WHERE bags >= 4")[0]['min'];
+            $bag5 = DB::query("SELECT MIN(dailyPrice) as 'min' FROM cartypes WHERE bags >= 5")[0]['min'];
+            $bag7 = DB::query("SELECT MIN(dailyPrice) as 'min' FROM cartypes WHERE bags >= 7")[0]['min'];
+
+
+            return $view->render($response, 'car_selection.html.twig', [
+                'allVehicles' => $allVehicles,
+                'carMinPrice' => $carMinPrice,
+                'suvMinPrice' => $suvMinPrice,
+                'vanMinPrice' => $vanMinPrice,
+                'truckMinPrice' => $truckMinPrice,
+                'pass2'=>$pass2,
+                'pass4'=>$pass4,
+                'pass5'=>$pass5,
+                'pass7'=>$pass7,
+                'bag3'=>$bag3,
+                'bag4'=>$bag4,
+                'bag5'=>$bag5,
+                'bag7'=>$bag7
             ]);
         });
 
 
-        $app->post('/review_reserve/{id:[0-9]+}', function (Request $request, Response $response, array $args){
+        $app->post('/review_reserve/{id:[0-9]+}', function (Request $request, Response $response, array $args) {
             $view = Twig::fromRequest($request);
             $selId = $args['id'];
 
             $selVehicle = DB::query("SELECT * FROM cartypes WHERE id = %s", $selId);
 
-            return $view->render($response, 'review_reserve.html.twig',[
-                'selVehicle'=>$selVehicle[0]
+            return $view->render($response, 'review_reserve.html.twig', [
+                'selVehicle' => $selVehicle[0]
             ]);
         });
 
 
-   /*     $app->post('/review_reserve', function (Request $request, Response $response, array $args){
-            $view = Twig::fromRequest($request);
+        /*     $app->post('/review_reserve', function (Request $request, Response $response, array $args){
+                 $view = Twig::fromRequest($request);
 
-        $jsonText = $request->getBody()->getContents();
+             $jsonText = $request->getBody()->getContents();
 
-           // $data = json_decode($jsonText,true);
+                // $data = json_decode($jsonText,true);
 
-            $result = DB::queryFirstRow("SELECT * FROM cartypes WHERE id = 2" );
-            $jsonData = json_encode($result);
-            return $response->getBody()->write($jsonData);
-        });*/
+                 $result = DB::queryFirstRow("SELECT * FROM cartypes WHERE id = 2" );
+                 $jsonData = json_encode($result);
+                 return $response->getBody()->write($jsonData);
+             });*/
 
 
     };
