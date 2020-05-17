c347d6986dc4c56e0d02c00c57ce8f8ba9515026
W <futurename@gmail.com>
2020-05-17 01:46:49 -0400

    add order expense summary chart

--- config/routes.php
+++ config/routes.php
@@ -271,12 +271,26 @@
             if (isset($_SESSION['userId'])) {
                 $userId = $_SESSION['userId'];
                 $orders = DB::query("SELECT * FROM orders WHERE userId = %s", $userId);
+                $rawData = DB::query("SELECT monthname(createdTS) as 'Month', 
+                                        year(createdTS) as 'Year',
+                                        SUM(totalPrice) as 'Exp'                                        
+                                        FROM orders 
+                                        WHERE userId=%s
+                                        GROUP BY month(createdTS),
+                                                 year(createdTS)
+                                        ORDER BY createdTS", $userId);
+
+                $dataPoints = [];
+                foreach ($rawData as $point){
+                    array_push($dataPoints,array('y'=>$point['Exp'],'label'=>$point['Month'].','.$point['Year']));
+                }
 
                 return $view->render($response, 'summary_orders.html.twig', [
                     'orders' => $orders,
                     'keyList' => [
                         'id', 'reservationId', 'userId', 'carId', 'createdTS', 'returnDateTime', 'totalPrice', 'rentStoreId', 'returnStoreId',
-                    ]
+                    ],
+                    'dataPoints'=>json_encode($dataPoints, JSON_NUMERIC_CHECK)
                 ]);
             } else {
                 return $view->render($response, 'login.html.twig', []);
